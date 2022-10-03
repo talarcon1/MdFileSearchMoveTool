dac72ea3151d275123021711e20d124eeaac7dc0
Tim Alarcon <t.alarcon1182@gmail.com>
2022-10-02 22:01:37 -0400

    Add files via upload

---
+++ MdFileSearchMoveTool.sh
@@ -0,0 +1,30 @@
+# ------------------------------------------------------------------------------------------
+# MdFileSearchMoveTool.sh automates the process to search the contents of the directory which
+# this script is in and move all found files to a specified directory
+# v1.0
+# ------------------------------------------------------------------------------------------
+
+mdFileNames=($(ls *.md))
+destination="./destination"
+declare -a foundFiles
+echo Enter search word:
+read searchWord
+searchWordUpper=$(echo $searchWord | tr '[:lower:]' '[:upper:]')
+echo ...searching...
+for i in "${!mdFileNames[@]}"
+do
+    fileContent=$(<${mdFileNames[$i]})
+    fileContentUpper=$(echo $fileContent | tr '[:lower:]' '[:upper:]'
+    if [[ $fileContentUpper == *$searchWordUpper* ]]; then
+        foundFiles+=(${mdFileNames[$i]})
+    fi
+done
+
+foundFilesLength=${#foundFiles[@]}
+echo "Files found: $foundFilesLength"
+for i in "${!foundFiles[@]}"
+do
+    echo "moving ${foundFiles[$i]} to $destination"
+    mv "./${foundFiles[$i]}" "$destination"
+done
+echo Done
